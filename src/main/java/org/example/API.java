93cfad764c30b63344cfb3e9df305bf79191fc72
IntesarAljabri <intesaraljabri99@gmail.com>
2023-04-10 13:50:44 +0400

    classAPI

--- src/main/java/org/example/API.java
+++ src/main/java/org/example/API.java
@@ -1,5 +1,34 @@
 package org.example;
 
 public class API {
+    String apiUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yY0PdPrB7EvhGB2kiMT8dP9PbAAyWiuN";
+        try {
+        URL url = new URL(apiUrl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Accept", "application/json");
 
+        if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+            throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+        }
+
+        BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+        StringBuilder json = new StringBuilder();
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            json.append(line);
+        }
+
+        conn.disconnect();
+
+        Gson gson = new Gson();
+        Countries[] countriesArr = gson.fromJson(json.toString(), Countries[].class);
+
+        // Do something with the countries array
+
+    } catch (RuntimeException ex) {
+        ex.printStackTrace();
+    }
 }
+
